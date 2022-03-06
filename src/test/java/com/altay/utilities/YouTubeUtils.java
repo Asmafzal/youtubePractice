66c64513fd970dd8a80128131d82ffbecd9c78f6
asma <asmafal@gmail.com>
2022-03-06 17:25:29 -0600

    US_LoginFunction.java updated

--- src/test/java/com/altay/utilities/YouTubeUtils.java
+++ src/test/java/com/altay/utilities/YouTubeUtils.java
@@ -1,6 +1,9 @@
 package com.altay.utilities;
 
 import com.github.javafaker.Faker;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.testng.Assert;
 
 public class YouTubeUtils {
 
@@ -13,6 +16,14 @@ public class YouTubeUtils {
     public static String fakePassword() {
         Faker faker = new Faker();
         return faker.bothify("??##??##??");
+    }
+
+    public static void login() {
+        Driver.getDriver().findElement(By.xpath("//div[@id='end']//tp-yt-paper-button[@id='button']")).click();
+        Driver.getDriver().findElement(By.id("identifierId")).sendKeys(ConfigurationReader.getProperty("username"));
+        Driver.getDriver().findElement(By.xpath("//span[.='Next']")).click();
+        Driver.getDriver().findElement(By.xpath("//div[@class='Xb9hP']//input[@type='password']")).sendKeys(ConfigurationReader.getProperty("password") + Keys.ENTER);
+       
     }
 
 
